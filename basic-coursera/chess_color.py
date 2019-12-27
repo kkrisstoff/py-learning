411c053e3e6753d98a3234ae203d630de815fa0d
Kostiantyn Pashura <kpash@softserveinc.com>
2019-12-27 18:16:13 +0200

    chess color

--- basic-coursera/chess_color.py
+++ basic-coursera/chess_color.py
@@ -1,14 +1,10 @@
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
+x1, y1 = (int(input()), int(input()))
+x2, y2 = (int(input()), int(input()))
 
 isBlack1 = (x1 % 2 == 0 and y1 % 2 == 0) or (x1 % 2 != 0 and y1 % 2 != 0)
 isBlack2 = (x2 % 2 == 0 and y2 % 2 == 0) or (x2 % 2 != 0 and y2 % 2 != 0)
 
-if isBlack1 and isBlack2:
-    print('YES')
-elif (not isBlack1) and (not isBlack2):
+if (isBlack1 and isBlack2) or (not isBlack1 and not isBlack2):
     print('YES')
 else:
     print('NO')
